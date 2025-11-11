cb8b212d9d78c6b2c88a7d9afc4a3ea283f42bbd
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-11-11 10:21:21 +0530

    Create urls.py

---
+++ tracker/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('add/', views.add_expense, name='add_expense'),
+    path('edit/<int:pk>/', views.edit_expense, name='edit_expense'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+]
